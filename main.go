495b7488628126b6fdd49c19592ee13903bf8c05
Deepak <er.d.ahuja@gmail.com>
2019-08-02 23:07:30 +0530

    add more layouts

--- main.go
+++ main.go
@@ -12,31 +12,40 @@ import (
 var (
 	homeView    *views.View
 	profileView *views.View
+	loginView   *views.View
+	signUpView  *views.View
 )
 
 func homeFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := homeView.Template.Execute(w, nil); err != nil {
-		panic(err)
-	}
+	must(homeView.Render(w, "index", nil))
 }
 
 func profileFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if err := profileView.Template.Execute(w, nil); err != nil {
-		panic(err)
-	}
+	must(profileView.Render(w, "index", nil))
+}
+
+func signUpFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(signUpView.Render(w, "index", nil))
+}
+
+func loginFunc(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	must(loginView.Render(w, "index", nil))
 }
 
 func pageNotFoundFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "not found")
+	fmt.Fprint(w, "Page not found")
 }
 
 func init() {
 	homeView = views.New("./views/home.gohtml")
 	profileView = views.New("./views/profile.gohtml")
+	signUpView = views.New("./views/signUp.gohtml")
 }
 
 func main() {
@@ -49,6 +58,14 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", homeFunc)
 	router.HandleFunc("/profile", profileFunc)
+	router.HandleFunc("/signup", signUpFunc)
+	router.HandleFunc("/login", loginFunc)
 	router.NotFoundHandler = http.HandlerFunc(pageNotFoundFunc)
 	http.ListenAndServe(":3000", router)
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
--- views/views.go
+++ views/views.go
@@ -1,7 +1,15 @@
 package views
 
-import "html/template"
+import (
+	"html/template"
+	"net/http"
+	"path/filepath"
+)
 
+var layoutDir = "views/layouts/"
+var templateExt = ".gohtml"
+
+// View type containing template and error
 type View struct {
 	Err      error
 	Template *template.Template
@@ -9,10 +17,26 @@ type View struct {
 
 // New parses a template for html
 func New(files ...string) *View {
-	files = append(files, "views/layouts/header.gohtml", "views/layouts/footer.gohtml")
+	files = append(files, layoutFiles()...)
 	tpl, err := template.ParseFiles(files...)
 	return &View{
 		Template: tpl,
 		Err:      err,
 	}
+}
+
+func layoutFiles() []string {
+	files, err := filepath.Glob(layoutDir + "*" + templateExt)
+	if err != nil {
+		panic(err)
+	}
+	return files
+}
+
+// Render the view
+func (v *View) Render(w http.ResponseWriter, lyt string, data interface{}) error {
+	if err := v.Template.ExecuteTemplate(w, lyt, nil); err != nil {
+		return (err)
+	}
+	return nil
 }
