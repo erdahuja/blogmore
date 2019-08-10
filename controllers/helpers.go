a9d81d0a44f051e296989faeddbda6b14a32181e
Deepak <er.d.ahuja@gmail.com>
2019-08-10 14:30:34 +0530

    add pretty print utility, env file

--- controllers/helpers.go
+++ controllers/helpers.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
 	schema "github.com/gorilla/Schema"
@@ -14,5 +16,16 @@ func parseForm(dest interface{}, r *http.Request) error {
 	if err := decoder.Decode(dest, r.PostForm); err != nil {
 		return err
 	}
+	return nil
+}
+
+// PrettyPrint variable (struct, map, array, slice) in Golang
+func PrettyPrint(v interface{}) (err error) {
+	b, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		fmt.Println("[PrettyPrint] Error while processing variable.")
+		return err
+	}
+	fmt.Println(string(b))
 	return nil
 }
